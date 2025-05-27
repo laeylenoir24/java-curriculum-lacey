68198ce8d0a7315a509c53844f329c431dc2ecc7
Ben Rohus <brohus@apprenticareers.org>
2025-05-27 11:17:30 -0500

    inital commit of repository

---
+++ 1-java-languge-fundamentals/exercises/DataTypesExercise.java
@@ -0,0 +1,84 @@
+public class DataTypesExercise {
+
+    public static void main(String[] args) {
+
+        /* 1. Declare and assign variables related to a soccer player */
+        // store the name of a soccer player.
+        // store the player's jersey number.
+        // store the player’s field position (e.g., "Goalkeeper")
+        // store whether the player is a starter (true or false)
+        // store the name of the player’s team
+        // -- use all of these variables in print out statements to the console
+        /* Example output:
+            Soccer player: Birgit Prinz
+            Jersey number: 9
+            Starter: True
+            Plays as a Forward for Germany.
+        */
+        // change the player's jersey number, if they are a starter, and their team's name
+        // reprint the previous console output with the new values
+
+        /* 2. Declare and assign variables related to a movie */
+        // store the title of a movie.
+        // store the year the movie was released.
+        // store the total number of reviews for a movie
+        // store the average of all reviews (scores 1 out of 10)
+        // store the name of the director of a film
+        // -- use all of these variables in print out statements to the console
+        /* Example output:
+            The movie Seven Samurai directed by Akira Kurosawa was released in 1954.
+            It has an IMDB score of 8.6/10 with 384819 reviews.
+        */
+        // update the number of reviews and average review score with new values
+        // reprint the previous console output with requested updates
+
+
+        /* 3. Declare and assign the following variables for a weather forecast: */
+        // store the name of a city.
+        // store the temperature in Fahrenheit.
+        // store the humidity percentage.
+        // store a short description of the weather (e.g., "Cloudy").
+        // -- use all of these variables in print out statements to the console
+        /* Example output:
+            Weather Report: Philadelphia
+            It is currently raining with a temperature of 63°F and a humidity of 44%
+        */
+        // change the temperature, humidity, and description of the weather
+        // reprint the previous console output with requested updates
+
+
+        /* 4.  Declare and assign the following variables for a flight at an airport: */
+        // store the flight number (e.g., "AA256").
+        // store the departure city (e.g., "New York").
+        // store the arrival city (e.g., "Los Angeles").
+        // store the seat number for the flight.
+        // store the terminal number at the airport.
+        // store whether the flight is delayed (true or false)
+        // -- use all of these variables in print out statements to the console
+        /* Example output:
+            Flight: AF21084 London to New York (NYC)
+            Gate: C
+            Seat: 8F
+            Delayed: False
+        */
+
+
+        /* 5. Declare and assign three boolean variables related to school or work. */
+        // whether you have homework (true or false).
+        // whether today is the weekend (true or false).
+        // whether you attended class today (true or false)
+        // -- these variables in print out statements to the console
+        /* Example output:
+            Do I have homework? False
+            Is it the weekend? False
+            Did I attend class today? True
+         */
+
+
+        /* 6. primitive data types */
+        // for each of the 8 primitive data types declare and
+        // initialize a variable that would store a real world value fitting for each data type
+        // byte, short, int, long, float, double, boolean, char
+
+    }
+}
---
+++ 1-java-languge-fundamentals/exercises/OperatorsExercise.java
@@ -0,0 +1,68 @@
+public class OperatorsExercise {
+
+    public static void main(String[] args) {
+
+        /* 1. Arithmetic Operations  */
+        // declare and initialize variables for each of the following
+        // number of hours in a day
+        // number of minutes in an hour
+        // number of seconds in a minute
+        // number of inches in a foot
+        // number of feet in a mile (5280)
+        // length of the movie Titanic in minutes (195 minutes)
+        //
+        // using the above variables and arithmetic operators (+ - / * %)
+        // print to the console solutions to the following prompts:
+        // number of hours in a week
+        // number of minutes in a week
+        // if a car is moving 77 miles per hour, how many feet per second is it traveling? (~112.933 seconds)
+        // how many hours and minutes long is the movie Titanic?
+        // a mile and 900 feet is how many inches?
+
+        /* 2. Compound assignment operators */
+        // demonstrate the use of all 5 compound assignment numbers with the variable x to print out the correct value
+        int x = 10;
+        // Same as x = x + 7; // print "x is 17"
+        // Same as x = x - 5; // print "x is 12"
+        // Same as x = x * 7; // print "x is 84"
+        // Same as x = x / 3; // print "x is 28"
+        // Same as x = x % 9; // print "x is 2"
+
+
+        /* 3. Complete the Conversion formulas  */
+        // complete the following formulas using temperature_conversion.jpg as a guide
+
+        double celsius = 32.0;
+        double kelvin = 350;
+        double fahrenheit = 212;
+
+        double celsiusToKelvin = 0; // ~305.15°K
+        System.out.println(celsius + "° Celsius = " + celsiusToKelvin + "° Kelvin");
+
+        double kelvinToCelsius = 0; // ~76.85°C
+        System.out.println(kelvin + "° Kelvin = " + kelvinToCelsius + "° Celsius");
+
+        double fahrenheitToCelsius = 0; // ~100°C
+        System.out.println(fahrenheit + "° Fahrenheit = " + fahrenheitToCelsius + "° Celsius");
+
+        double celsiusToFahrenheit = 0; // ~ 89.6°K
+        System.out.println(celsius + "° Celsius = " + celsiusToFahrenheit + "° Kelvin");
+
+        double fahrenheitToKelvin = 0; // ~373.15°K
+        System.out.println(fahrenheit + "° Fahrenheit = " + fahrenheitToKelvin + "° Kelvin");
+
+        double kelvinToFahrenheit = 0; // ~170.33°F
+        System.out.println(kelvin + "° Kelvin = " + kelvinToFahrenheit + "° Fahrenheit");
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
