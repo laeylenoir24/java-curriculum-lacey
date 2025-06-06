3d4c304d98a4e1600df35926eccb146e88ae5d73
lacey <lacey.lenoir@gmail.com>
2025-06-06 15:45:20 -0400

    laceylen-java

---
+++ 1-java-languge-fundamentals/exercises/JavaSummative.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class JavaSummative {
+    private static final int totalLockers = 10;
+    private static final int pinLength = 4;
+    private static final int maxPin = (int) Math.pow(10, pinLength);
+    private static String[] lockers = new String[totalLockers];
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nLocker System - Main Menu");
+
+            boolean lockersAvail = hasAvailLocker();
+            int optionNumber = 1;
+
+            if (lockersAvail) {
+                System.out.println(optionNumber++ + ". Rent a locker");
+            }
+
+            System.out.println(optionNumber++ + ". Open a locker");
+            System.out.println(optionNumber++ + ". Release a locker");
+            System.out.println(optionNumber + ". Exit");
+
+            System.out.println("Select an option: ");
+            String input = scanner.nextLine();
+
+            try {
+                int choice = Integer.parseInt(input);
+                int rentOpt = lockersAvail ? 1 : -1;
+                int openOpt = lockersAvail ? 2 : 1;
+                int releaseOpt = lockersAvail ? 3 : 2;
+                int exitOpt = lockersAvail ? 4 : 3;
+
+                if (choice == rentOpt && lockersAvail) {
+                    rentLocker();
+                } else if (choice == openOpt) {
+                    openLocker(scanner);
+                } else if (choice == releaseOpt) {
+                    releaseLocker(scanner);
+                } else if (choice == exitOpt) {
+                    System.out.println("Exiting...");
+                    return;
+                } else {
+                    System.out.println("Invalid option");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Enter a number");
+            }
+        }
+    }
+}
