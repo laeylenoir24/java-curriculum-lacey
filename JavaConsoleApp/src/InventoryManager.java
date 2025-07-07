e5f02ed63233b1fd13052247f704d80f374f8d5e
lacey <lacey.lenoir@gmail.com>
2025-07-07 00:02:40 -0400

    laceylen-java

---
+++ JavaConsoleApp/src/InventoryManager.java
@@ -0,0 +1,27 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+
+public class InventoryManager {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> productName = new ArrayList<>();
+
+    }
+
+    public static class product {
+        private static int productID;
+        private static String productName;
+        private static int quantity;
+        private static BigDecimal price;
+
+
+
+
+
+
+
+    }
+}
